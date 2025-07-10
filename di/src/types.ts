e285ef639b76e78b18f5473534c77bae668ca291
kimyu0218 <serena35@naver.com>
2025-07-10 23:56:53 +0900

    refactor: 타입 분리

--- di/src/types.ts
+++ di/src/types.ts
@@ -1,2 +1,10 @@
-export type BeanClass = string;
+export type BeanKey = string | symbol;
 export type Profile = string;
+export type Constructor<T = any> = new (...args: any[]) => T;
+
+export interface BeanParameter {
+  name?: string;
+  value?: string;
+}
+
+export interface Autowired {}
