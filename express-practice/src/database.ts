d4c0360aa4778e17a4f5daa64cbcf508bafb9b7a
kimyu0218 <serena35@naver.com>
2025-08-16 14:19:44 +0900

    refactor: [sequelize] 커넥션풀 설정 추가
    - DB 커넥션을 재사용하여 효율성 개선
    - 커넥션을 얻기 위해 최대 10초 대기
    - 사용하지 않는 커넥션은 10초 후 풀에서 제거

--- express-practice/src/database.ts
+++ express-practice/src/database.ts
@@ -9,6 +9,12 @@ export const sequelize =
         username: process.env.MYSQL_USERNAME || 'root',
         host: process.env.MYSQL_HOST || 'localhost',
         port: (process.env.MYSQL_PORT as unknown as number) || 3306,
+        pool: {
+          max: 5,
+          min: 0,
+          acquire: 10000,
+          idle: 10000,
+        },
       });
 
 export async function initTestDatabase() {
